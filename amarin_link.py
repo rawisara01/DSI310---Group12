5ae1c0503df9264c9e4736406c1f2ad2480b9abe
Rawisara Bundith <70097607+rawisara01@users.noreply.github.com>
2022-10-26 13:27:21 +0700

    Update amarin_link.py

--- amarin_link.py
+++ amarin_link.py
@@ -4,16 +4,12 @@ import re
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
-# https://www.amarintv.com/tags/%E0%B8%AD%E0%B8%B8%E0%B8%9A%E0%B8%B1%E0%B8%95%E0%B8%B4%E0%B9%80%E0%B8%AB%E0%B8%95%E0%B8%B8
-# https://www.amarintv.com/news/detail/153044
-
 class linkSpider(CrawlSpider):
     name = "link"
     allowed_domains = ['amarintv.com']
     start_urls = ['https://www.amarintv.com/tags/%E0%B8%AD%E0%B8%B8%E0%B8%9A%E0%B8%B1%E0%B8%95%E0%B8%B4%E0%B9%80%E0%B8%AB%E0%B8%95%E0%B8%B8',]
 
     rules = (
-        # , deny='detail'
         Rule(LinkExtractor(allow='tags/%E0%B8%AD%E0%B8%B8%E0%B8%9A%E0%B8%B1%E0%B8%95%E0%B8%B4%E0%B9%80%E0%B8%AB%E0%B8%95%E0%B8%B8')),
         Rule(LinkExtractor(allow='detail'), callback='parse_links')
     )
@@ -25,7 +21,3 @@ class linkSpider(CrawlSpider):
             'date' : response.css('div.option::text').get(),
             'tags' : response.css('strong.tag a::text').get(),
         }
-
-# response.css('div.option::text').get()
-# response.css('div.head h1::text').get()
-# response.css('strong.tag a::text').getall()
